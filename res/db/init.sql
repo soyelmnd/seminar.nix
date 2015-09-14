64a2becf37d5c3f65ea6d045e64d7a1dd8b98c65
elmnd <minhnd.it@gmail.com>
2015-09-14 16:55:01 +0700

    DB pushed

---
+++ res/db/init.sql
@@ -0,0 +1,8 @@
+-- Photo
+-- Actually this is just dummy
+--   as we haven't setup any DB yet
+CREATE TABLE IF NOT EXISTS `photo` (
+  `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  `title` TEXT,
+  `uri` TEXT
+);
